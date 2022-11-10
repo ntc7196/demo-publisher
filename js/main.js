4a7fae89ed864181ab108c879820a66b0fae72aa
ntc7196 <ntc.7196@gmail.com>
2022-11-11 03:10:02 +0700

    init

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-console.log('hello123')
-const swiper = new Swiper('.swiper', {
-  speed: 400,
-  spaceBetween: 100,
+const header = $('header')
+var lastScrollTop = 0
+$(window).scroll(function () {
+  const height = $(window).scrollTop()
+  if (height > lastScrollTop) {
+    const fixHeaderH = header.innerHeight() - 90
+    if (height >= fixHeaderH) {
+        $('.header-menu').addClass('bg-dark')
+    }
+  } else {
+    if (height <= 430-90) {
+        $('.header-menu').removeClass('bg-dark')
+    }
+  }
+  lastScrollTop = height
 })
